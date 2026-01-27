b621bfa09ee1601b76edd86e54ba6611c548f95b
Shivam-is <168400729+Shivam-is@users.noreply.github.com>
2026-01-28 00:12:30 +0530

    Duplicate Republic Day greeting message

--- RepublicDay.java
+++ RepublicDay.java
@@ -1,6 +1,7 @@
 public class RepublicDay {
     public static void main(String[] args) {
         System.out.println("Happy Republic Day to everyone!");
+        System.out.println("Happy Republic Day to everyone!");
         System.out.println("Wishing you from my heart unity, freedom, and progress.");
     }
 }
