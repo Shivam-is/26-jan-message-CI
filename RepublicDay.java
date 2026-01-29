d4413eb275f954b07a3955649c407a2119b6eac7
Shivam-is <168400729+Shivam-is@users.noreply.github.com>
2026-01-30 01:00:10 +0530

    Update RepublicDay.java

--- RepublicDay.java
+++ RepublicDay.java
@@ -2,7 +2,7 @@ public class RepublicDay {
     public static void main(String[] args) {
         System.out.println("Happy Republic Day to everyone!");
         System.out.println("Happy Republic Day to everyone!");
-        System.out.println("Happy Republic Day to everyone!");
+        System.out.println("Happy Republic Day to my sister!");
         System.out.println("Happy Republic Day to shivam!");
         System.out.println("Wishing you from my heart unity, freedom, and progress.");
     }
